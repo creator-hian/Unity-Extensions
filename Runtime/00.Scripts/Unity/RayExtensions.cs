9b8c5a80446ebb17092fcdd3b418d82c51ba693c
FAMOZ-MINWOO\FAMOZ_Minwoo <shahian12@gmail.com>
2024-12-19 11:38:00 +0900

    ✨ feat: Add Ray extension methods
    Adds a set of extension methods to the `Ray` struct to provide
    additional functionality, such as reversing the ray, randomly
    deflecting the ray direction, and more. These extensions
    enhance the usability and flexibility of the `Ray` struct
    in various Unity-based applications.

---
+++ Runtime/00.Scripts/Unity/RayExtensions.cs
@@ -0,0 +1,78 @@
+using UnityEngine;
+
+namespace Hian.Extensions
+{
+    /// <summary>
+    /// Ray 구조체에 대한 확장 메서드를 제공합니다.
+    /// </summary>
+    public static class RayExtensions
+    {
+        /// <summary>
+        /// 반전된 Ray를 생성합니다.
+        /// </summary>
+        /// <param name="ray">대상 Ray.</param>
+        /// <param name="maxDistance">Ray의 최대 거리.</param>
+        /// <returns>반전된 Ray.</returns>
+        public static Ray Reversed(this Ray ray, float maxDistance) =>
+            new Ray(ray.GetPoint(maxDistance), -ray.direction);
+
+        /// <summary>
+        /// 주어진 <paramref name="angle"/>만큼 임의로 Ray 방향을 편향시킵니다.
+        /// </summary>
+        /// <param name="ray">대상 Ray.</param>
+        /// <param name="angle">Ray가 편향될 각도.</param>
+        /// <returns>편향된 Ray.</returns>
+        public static Ray RandomDeflected(this Ray ray, float angle) =>
+            RandomDeflected(ray, new Vector2(angle, angle), Vector3.up);
+
+        /// <summary>
+        /// <paramref name="up"/> 축을 고려하여 주어진 <paramref name="angle"/>만큼 임의로 Ray 방향을 편향시킵니다.
+        /// </summary>
+        /// <param name="ray">대상 Ray.</param>
+        /// <param name="angle">Ray가 편향될 각도.</param>
+        /// <param name="up">위쪽 축.</param>
+        /// <returns>편향된 Ray.</returns>
+        public static Ray RandomDeflected(this Ray ray, float angle, Vector3 up) =>
+            RandomDeflected(ray, new Vector2(angle, angle), up);
+
+        /// <summary>
+        /// 주어진 <paramref name="angleX"/>와 <paramref name="angleY"/>만큼 임의로 Ray 방향을 편향시킵니다.
+        /// </summary>
+        /// <param name="ray">대상 Ray.</param>
+        /// <param name="angleX">X축을 기준으로 Ray가 편향될 각도.</param>
+        /// <param name="angleY">Y축을 기준으로 Ray가 편향될 각도.</param>
+        /// <returns>편향된 Ray.</returns>
+        public static Ray RandomDeflected(this Ray ray, float angleX, float angleY) =>
+            RandomDeflected(ray, new Vector2(angleX, angleY), Vector3.up);
+
+        /// <summary>
+        /// <paramref name="up"/> 축을 고려하여 주어진 <paramref name="angleX"/>와 <paramref name="angleY"/>만큼 임의로 Ray 방향을 편향시킵니다.
+        /// </summary>
+        /// <param name="ray">대상 Ray.</param>
+        /// <param name="angleX">X축을 기준으로 Ray가 편향될 각도.</param>
+        /// <param name="angleY">Y축을 기준으로 Ray가 편향될 각도.</param>
+        /// <param name="up">위쪽 축.</param>
+        /// <returns>편향된 Ray.</returns>
+        public static Ray RandomDeflected(this Ray ray, float angleX, float angleY, Vector3 up) =>
+            RandomDeflected(ray, new Vector2(angleX, angleY), up);
+
+        /// <summary>
+        /// 주어진 <paramref name="angles"/>만큼 임의로 Ray 방향을 편향시킵니다.
+        /// </summary>
+        /// <param name="ray">대상 Ray.</param>
+        /// <param name="angles">X축과 Y축을 기준으로 Ray가 편향될 각도.</param>
+        /// <returns>편향된 Ray.</returns>
+        public static Ray RandomDeflected(this Ray ray, Vector2 angles) =>
+            RandomDeflected(ray, angles, Vector3.up);
+
+        /// <summary>
+        /// <paramref name="up"/> 축을 고려하여 주어진 <paramref name="angles"/>만큼 임의로 Ray 방향을 편향시킵니다.
+        /// </summary>
+        /// <param name="ray">대상 Ray.</param>
+        /// <param name="angles">X축과 Y축을 기준으로 Ray가 편향될 각도.</param>
+        /// <param name="up">위쪽 축.</param>
+        /// <returns>편향된 Ray.</returns>
+        public static Ray RandomDeflected(this Ray ray, Vector2 angles, Vector3 up) =>
+            new Ray(ray.origin, ray.direction.RandomDeflected(angles, up));
+    }
+}
