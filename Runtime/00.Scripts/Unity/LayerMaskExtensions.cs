637b9a4d98700f10a4e4e8b7094e4a769268eaa6
FAMOZ-MINWOO\FAMOZ_Minwoo <shahian12@gmail.com>
2024-12-19 14:54:52 +0900

    🎨 feat(LayerMaskExtensions): Add extension methods for managing layer masks
    This commit introduces a new `LayerMaskExtensions` class that provides a set of extension methods for working with Unity's `LayerMask` type. The main features include:
    
    - `WithLayers(int[] layers)` and `WithLayers(string[] layerNames)` methods to add layers to a layer mask
    - `WithoutLayers(int[] layers)` and `WithoutLayers(string[] layerNames)` methods to remove layers from a layer mask
    
    These extensions make it easier to manage layer masks programmatically, which is a common task in Unity game development.

---
+++ Runtime/00.Scripts/Unity/LayerMaskExtensions.cs
@@ -0,0 +1,76 @@
+using UnityEngine;
+
+namespace Hian.Extensions
+{
+    /// <summary>
+    /// LayerMask에 대한 확장 메서드를 제공합니다.
+    /// </summary>
+    public static class LayerMaskExtensions
+    {
+        /// <summary>
+        /// 레이어 마스크에 레이어를 추가합니다.
+        /// </summary>
+        /// <param name="layerMask">레이어를 추가할 레이어 마스크.</param>
+        /// <param name="layers">추가할 레이어.</param>
+        /// <returns>업데이트된 레이어 마스크.</returns>
+        public static LayerMask WithLayers(this LayerMask layerMask, params int[] layers)
+        {
+            foreach (var layer in layers)
+            {
+                layerMask |= 1 << layer;
+            }
+
+            return layerMask;
+        }
+
+        /// <summary>
+        /// 레이어 마스크에 레이어를 추가합니다.
+        /// </summary>
+        /// <param name="layerMask">레이어를 추가할 레이어 마스크.</param>
+        /// <param name="layerNames">추가할 레이어의 이름.</param>
+        /// <returns>업데이트된 레이어 마스크.</returns>
+        public static LayerMask WithLayers(this LayerMask layerMask, params string[] layerNames)
+        {
+            foreach (var name in layerNames)
+            {
+                var layer = LayerMask.NameToLayer(name);
+                layerMask |= 1 << layer;
+            }
+
+            return layerMask;
+        }
+
+        /// <summary>
+        /// 레이어 마스크에서 레이어를 제거합니다.
+        /// </summary>
+        /// <param name="layerMask">레이어를 제거할 레이어 마스크.</param>
+        /// <param name="layers">제거할 레이어.</param>
+        /// <returns>업데이트된 레이어 마스크.</returns>
+        public static LayerMask WithoutLayers(this LayerMask layerMask, params int[] layers)
+        {
+            foreach (var layer in layers)
+            {
+                layerMask &= ~(1 << layer);
+            }
+
+            return layerMask;
+        }
+
+        /// <summary>
+        /// 레이어 마스크에서 레이어를 제거합니다.
+        /// </summary>
+        /// <param name="layerMask">레이어를 제거할 레이어 마스크.</param>
+        /// <param name="layerNames">제거할 레이어의 이름.</param>
+        /// <returns>업데이트된 레이어 마스크.</returns>
+        public static LayerMask WithoutLayers(this LayerMask layerMask, params string[] layerNames)
+        {
+            foreach (var name in layerNames)
+            {
+                var layer = LayerMask.NameToLayer(name);
+                layerMask &= ~(1 << layer);
+            }
+
+            return layerMask;
+        }
+    }
+}
