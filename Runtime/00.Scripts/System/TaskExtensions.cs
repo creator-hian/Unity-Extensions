fd4c4da5c11b4afffede11fe54855fa62dabfa9e
FAMOZ-MINWOO\FAMOZ_Minwoo <shahian12@gmail.com>
2024-12-31 18:11:12 +0900

    Add Forget method to TaskExtensions for exception handling in async tasks
    - Introduced a new Forget method that allows tasks to be executed without propagating exceptions, providing an optional action for handling exceptions if desired.
    - Improved code clarity by removing unnecessary comments and enhancing documentation for the new method.
    - Maintained existing functionality while adding flexibility for developers managing asynchronous operations.

--- Runtime/00.Scripts/System/TaskExtensions.cs
+++ Runtime/00.Scripts/System/TaskExtensions.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -10,7 +11,7 @@ namespace Hian.Extensions
             CancellationToken cancellationToken
         )
         {
-            TaskCompletionSource<object> tcs = new TaskCompletionSource<object>(); // 불필요한 bool 대신 object 사용
+            TaskCompletionSource<object> tcs = new TaskCompletionSource<object>();
             using (
                 cancellationToken.Register(
                     static s => ((TaskCompletionSource<object>)s).TrySetResult(null),
@@ -20,11 +21,31 @@ namespace Hian.Extensions
             {
                 if (task != await Task.WhenAny(task, tcs.Task))
                 {
-                    throw new TaskCanceledException(task); // OperationCanceledException 대신 TaskCanceledException 사용
+                    throw new TaskCanceledException(task);
                 }
             }
 
             return await task;
+        }
+
+        /// <summary>
+        /// 태스크를 잊어버리도록 표시합니다. 이는 태스크에서 발생하는 모든 예외가 캐치되고 처리됨을 의미합니다.
+        /// </summary>
+        /// <param name="task">잊어버릴 태스크입니다.</param>
+        /// <param name="onException">예외가 발생했을 때 실행할 선택적 액션입니다. 제공된 경우 예외가 다시 발생하지 않습니다.</param>
+        public static async void Forget(this Task task, Action<Exception> onException = null)
+        {
+            try
+            {
+                await task;
+            }
+            catch (Exception exception)
+            {
+                if (onException == null)
+                    throw exception;
+
+                onException(exception);
+            }
         }
     }
 }
